1511fdb19f62b44eeb37cc17ba1f00e7b8d895ba
AymanBerraJ <166770154+AymanBerraJ@users.noreply.github.com>
2024-07-30 09:35:05 +0200

    createJob

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/login', (req, res) => {
     res.render('login');
 });
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+app.get('/createjob', (req, res) => {
+    res.render('createjob');
+});
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+app.get('/profile', (req, res) => {
+    res.render('profile');
+});
+app.get('/job', (req, res) => {
+    res.render('job');
+});
 
 
 app.listen(port, () => {
