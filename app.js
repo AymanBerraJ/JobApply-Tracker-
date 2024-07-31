432e7e0eda87bb51a27e495c9177223c96a113da
AymanBerraJ <166770154+AymanBerraJ@users.noreply.github.com>
2024-07-31 10:42:06 +0200

    ajout des routes/middle/connection db/cookie/models

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
+// const authRoutes = require("./routes/authRoutes");
+// const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 const port = 3000;
 
-
+// import ejs
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-app.get('/register', (req, res) => {
-    res.render('register');
-});
-app.get('/createjob', (req, res) => {
-    res.render('createjob');
-});
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard');
-});
-app.get('/profile', (req, res) => {
-    res.render('profile');
-});
-app.get('/job', (req, res) => {
-    res.render('job');
-});
+// database connection
 
+const dbURI =
+  "mongodb+srv://beayman35:rigmC9v8rATam6v3@cluster0.ewewown.mongodb.net/jobApply";
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/createjob", (req, res) => {
+  res.render("createjob");
+});
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+app.get("/job", (req, res) => {
+  res.render("job");
 });
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check if the token exists and verified
+  if (token) {
+    jwt.verify(token, "bdma secret", (err, decodedToken) => {
+      if (err) {
+        res.redirect("/login");
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+module.exports = { requireAuth };
+
+// check current User
+
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, "bdma secret", async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
